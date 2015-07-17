bc34a56aed4f86ae00f3b783b1624b257a6049b2
lanomarks <lanomarks@gmail.com>
2015-07-17 17:10:54 -0300

    First Commit.

---
+++ Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Quiron.LojaVirtual.UnitTest
+{
+    /// <summary>
+    /// Summary description for UnitTestQuiron
+    /// </summary>
+    [TestClass]
+    public class UnitTestQuiron
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
