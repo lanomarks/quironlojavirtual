8367853c40f71ca4cf2fb7ba7384af68a4878e1d
lanomarks <lanomarks@gmail.com>
2015-07-21 13:41:57 -0300

    aula06

--- Quiron.LojaVirtual.Dominio/Entidade/Produtos.cs
+++ Quiron.LojaVirtual.Dominio/Entidade/Produtos.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Quiron.LojaVirtual.Dominio.Entidade
 {
-    public class Produto
+    [Table("Produtos")]
+    public class Produtos
     {
-        public int ProdutoId { get; set; }
+        [Key]
+        public int? ProdutoId { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        public decimal Preco { get; set; }
+        public decimal? Preco { get; set; }
         public string Categoria { get; set; }
     }
 }
---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produtos> Produtos { get; set; }
+
+        /// <summary>
+        /// Removendo a pluralização do entity framework
+        /// e inserindo a do programador.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<EfDbContext>());
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<IncludeMetadataConvention>();
+            modelBuilder.Entity<Produtos>().ToTable("Produtos");
+        }
+    }
+}
