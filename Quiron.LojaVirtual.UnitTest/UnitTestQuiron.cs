825cc159466190820a5b2f9aabe3470f91536d34
lanomarks <lanomarks@gmail.com>
2015-07-21 15:31:26 -0300

    Realizando o TDD da paginação.

--- Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
@@ -1,19 +1,81 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
-    /// <summary>
-    /// Summary description for UnitTestQuiron
-    /// </summary>
     [TestClass]
     public class UnitTestQuiron
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Take()
         {
+            //O operador Take é usado para selecionar os primeiros n objetos de uma coleção.
+
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var resultado = from num in numeros.Take(5) select num;
+
+            int[] teste = { 5, 4, 1, 3, 9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+
+        }
+
+
+        [TestMethod]
+        public void Skip()
+        {
+
+            //O operador Skip ignora o(s) primeiro(s) n objetos de uma coleção.
+
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
+
+            int[] teste = { 1, 3, 9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+
+        }
+
+
+
+        [TestMethod]
+        public void TestarSeAPaginacaoEstaSendoGeradaCorretamente()
+        {
+            #region [-- Arrange --]
+
+            HtmlHelper htmlHelper = null;
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensTotal = 28,
+                ItensPorPagina = 10
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            #endregion
+
+            #region [-- Act --]
+
+            MvcHtmlString resultado = htmlHelper.PageLinks(paginacao, paginaUrl);
+
+            #endregion
+
+            #region [-- Assert --]
+
+            Assert.AreEqual(
+                    @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+                 + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+                 + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+                 );
+
+            #endregion
         }
     }
 }
