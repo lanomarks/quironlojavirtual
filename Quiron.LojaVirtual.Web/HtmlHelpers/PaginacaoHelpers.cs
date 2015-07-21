2ab47f38d2a0b6249d4eeb747918ce16a5ec2698
lanomarks <lanomarks@gmail.com>
2015-07-21 14:23:20 -0300

    Criada a paginação

---
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,42 @@
+﻿using Quiron.LojaVirtual.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+        //TOTAL DE ELEMENTOS POR PÁGINA IGUAL A 3
+        /// <summary>
+        /// Gerador de paginação
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="paginacao"></param>
+        /// <param name="paginaUrl"></param>
+        /// <returns></returns>
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1; i < paginacao.TotalPaginas; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+            }
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
